f334d33d3c3fbcb0102a48ecd3008ad1099f9f94
turner <csusbdt@gmail.com>
2021-08-21 20:43:34 -1000

    X

--- docs/grid.js
+++ docs/grid.js
@@ -26,8 +26,63 @@ class Grid {
 		this.box.draw();
 	}
 
+	cups_at(c, r) {
+		const cups = [null, null, null];
+		this.cups.forEach(cup => {
+			if (cup.c === c && cup.r === r) {
+				cups[cup.size] = cup;
+			}
+		});
+		return cups;
+	}
+
 	click(c, r) {
+		let dir = null;
 		if (this.box.c === c + 1 && this.box.r === r) {
+			dir = LEFT;
+		} else if (this.box.c === c - 1 && this.box.r === r) {
+			dir = RIGHT;
+		} else if (this.box.c === c && this.box.r === r + 1) {
+			dir = UP;
+		} else if (this.box.c === c && this.box.r === r - 1) {
+			dir = DOWN;
+		} else {
+			return;
+		}
+		const arriving_cups = [];
+
+		let cups = this.cups_at(c, r);
+		if (cups[2]) {
+			if (cups[2].dir !== dir) {
+				arriving
+			}
+		}
+
+
+		for (let i = 0; i < this.cups.length; ++i) {
+			const cup = this.cups[i];
+			if (cup.c === this.box.c && cup.r === this.box.r) {
+				
+			}
+		}
+
+		this.cups_at(c, r).filter(cup => {
+			if (cup === null) return false;
+			if (dir === LEFT) {
+			}
+		});
+		for (let i = 0; i < this.cups.length; ++i) {
+			const cup = this.cups[i];
+			if (cup.c === this.box.c && cup.r === this.box.r) {
+				
+			}
+		}
+
+
+		const receiving_cups = this.cups.map(cup => cup.c === c && cup.r === r);
+
+		if (this.box.c === c + 1 && this.box.r === r) {
+
 			console.log('go left');
 		} else if (this.box.c === c - 1 && this.box.r === r) {
 			console.log('go right');
